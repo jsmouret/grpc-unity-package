1a048a0b561bd91df4fb6729f66c414bf2ad9e08
Jean-Sebastien Mouret <js.mouret@gmail.com>
2018-07-13 15:05:06 +0800

    Add example unity project

---
+++ example/UnityGrpcClient/Assets/Scripts/Editor/BuildHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+
+public class BuildHelper
+{
+    // Add libresolv.tdb when building for iOS
+    // Thanks to https://gist.github.com/eppz/1ebbc1cf6a77741f56d63d3803e57ba3
+
+    [PostProcessBuildAttribute(1)]
+    public static void OnPostProcessBuild(BuildTarget target, string path)
+    {
+        if (target == BuildTarget.iOS)
+        {
+            var projectPath = PBXProject.GetPBXProjectPath(path);
+            var project = new PBXProject();
+            project.ReadFromString(File.ReadAllText(projectPath));
+            var targetGUID = project.TargetGuidByName(PBXProject.GetUnityTargetName());
+
+            project.AddFrameworkToProject(targetGUID, "libresolv.tbd", false);
+
+            File.WriteAllText(projectPath, project.WriteToString());
+        }
+    }
+}
---
+++ example/UnityGrpcClient/Assets/Scripts/Example.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using Grpc.Core;
+using Helloworld;
+
+public class Example : MonoBehaviour {
+
+    public Text Response;
+
+    public void Send()
+    {
+        try
+        {
+            var channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
+
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = client.SayHello(new HelloRequest { Name = "Unity" });
+
+            Response.text = reply.Message;
+
+            channel.ShutdownAsync().Wait();
+        }
+        catch (Exception e)
+        {
+            Response.text = "exception: " + e.Message;
+        }
+    }
+}
